c08b2f86581f60dcdef31c524e404f46f755f536
Neeraj Kumar <neerajkumar@Neerajs-MacBook-Air.local>
2026-01-10 12:11:08 +0530

    first commit

---
+++ demo.js
@@ -0,0 +1,45 @@
+// var A =  symbol("raunak");
+// console.log (A);
+// console.log("raunak bamotra \n i live in rajouri garden \n i am 18 year old \n i am studying in tips college" );
+// console.log(typeof(a));
+
+
+// var arr = {1,2,3,4,'raunak',true};
+// var arr2 = {...arr}
+// let newslice
+// start function 
+function hello(){
+console.log("hello world");
+console.log("ffeffewfweffwefewfewfewwefwefwe");
+}
+function greet(name){
+    console.log("hello"+ name);
+}
+greet("raunak");
+
+
+function sum(a,b){
+    console.log(a+b);
+}
+sum(10,10);
+
+
+let marks = 99;
+if(marks>=98){
+    console.log("A+ grade");
+}
+else if (marks>=80){
+    console.log("a grade");
+}
+else if (marks>=70){
+    console.log("B+ grade");
+}
+else if (marks>=75){
+    console.log("b grade");
+}
+else if (marks>=45){
+    console.log("c grade");
+}
+else{
+    console.log("fail");
+}
